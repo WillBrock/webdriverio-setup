6ff49a88b009803a115a677c7375579c6c62bcce
Will Brock <wpbrock@gmail.com>
2018-09-12 20:10:23 -0400

    Add selector examples

---
+++ tests/selectors.test.js
@@ -0,0 +1,16 @@
+describe(`Overview ofelectors`, () => {
+    it(`should verify selectors`, () => {
+        browser.url(`https://the-internet.herokuapp.com/`);
+
+        //$(`h1.heading`).getText();
+        //$(`heading`).getText();
+
+        //$(`h2=Available Examples`).getText()
+
+        //$(`a=Broken Images`).click();
+        //$(`a*=Images`).click();
+
+        //$(`ul`).$(`..`).getText();
+        //$(`ul`).$(`a=Checkboxes`).click();
+    });
+});
